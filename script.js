9738cad4a58c16301e033af1deb0722edd07c136
tblouir <tblouirschool@gmail.com>
2022-07-30 22:55:46 -0600

    Add basic math functionality and calculator CSS styling

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-let one = document.querySelector('.one')
-let two = document.querySelector('.two')
-let three = document.querySelector('.three')
-let four = document.querySelector('.four')
-let five = document.querySelector('.five')
-let six = document.querySelector('.six')
-let seven = document.querySelector('.seven')
-let eight = document.querySelector('.eight')
-let nine = document.querySelector('.nine')
-let zero = document.querySelector('.zero')
+// let one = document.querySelector('.one')
+// let two = document.querySelector('.two')
+// let three = document.querySelector('.three')
+// let four = document.querySelector('.four')
+// let five = document.querySelector('.five')
+// let six = document.querySelector('.six')
+// let seven = document.querySelector('.seven')
+// let eight = document.querySelector('.eight')
+// let nine = document.querySelector('.nine')
+// let zero = document.querySelector('.zero')
 let decimal = document.querySelector('.decimal')
 let clear = document.querySelector('.clear')
 let add = document.querySelector('.add')
@@ -17,47 +17,123 @@ let divide = document.querySelector('.divide')
 let submit = document.querySelector('.submit')
 let numberLine = document.querySelector('.number-line h1')
 let buttons = document.querySelectorAll('.button')
-let previousValue = 0;
+let lastOperator;
+let currentValue = 0;
+let runningValue = 0;
+let oneDecimal = true;
+let pressTwice = false;
 let array = [];
 
 buttons.forEach(button => button.addEventListener('click', e => {
-    if (parseFloat(e.target.textContent) > 0) {
-        array.push(e.target.textContent);
-        number = parseFloat(array.join(''));
-        numberLine.textContent = number;
+    if (typeof parseFloat(e.target.textContent) === 'number' || e.target === decimal) {
+        if (oneDecimal || typeof parseFloat(e.target.textContent) === 'number') {
+            array.push(e.target.textContent);
+            if (array[0] === '.') {
+                array.unshift(0);
+                number = parseFloat(array.join(''));
+                currentValue = number;
+                numberLine.textContent = `0.${number}`;
+            } else {
+                number = parseFloat(array.join(''));
+                currentValue = number;
+                numberLine.textContent = number;
+            }
+        }
+        if (e.target === decimal) {
+            oneDecimal = false;
+        }
     }
+
     if (e.target.textContent === 'Clear') {
-        number = 0;
-        previousValue = 0;
-        array.splice(0, array.length);
+        clearArray();
         numberLine.textContent = 0;
+        previousValue = 0;
+        currentValue = 0;
+        runningValue = 0;
+        pressTwice = false;
+        oneDecimal = true;
     }
-    switch (e.target.textContent) {
-        case '+':
-            console.log('add');
-            break;
-        case '-':
-            console.log('subtract');
-            break;
-        case 'x':
-            console.log('multiply');
-            break;
-        case '/':
-            console.log('divide');
-            break;
+
+    if (e.target === add || e.target === subtract || e.target === multiply || e.target === divide) {
+        
+        if (lastOperator === undefined) {
+            lastOperator = e.target;
+        };
+
+        if (pressTwice) {
+            switch (lastOperator) {
+                case add:
+                    calculateNumber(addition);
+                    break;
+                case subtract:
+                    calculateNumber(subtraction);
+                    break;
+                case multiply:
+                    calculateNumber(multiplication);
+                    break;
+                case divide:
+                    calculateNumber(division);
+                    break;
+                default:
+                    console.log("DEFAULT")
+            }
+        };
+
+        if (runningValue === 0) {
+            runningValue = currentValue;
+        };
+
+        clearArray();
+        pressTwice = true;
+    }
+
+    if (e.target === submit) {
+        switch (lastOperator) {
+            case add:
+                calculateNumber(addition);
+                break;
+            case subtract:
+                calculateNumber(subtraction);
+                break;
+            case multiply:
+                calculateNumber(multiplication);
+                break;
+            case divide:
+                calculateNumber(division);
+                break;
+            default:
+                console.log("DEFAULT")
+        }
     }
 })
-)
+);
 
-function addition(num) {
-    return previousValue += num;
+
+
+function addition() {
+    return runningValue += currentValue;
 }
-function subtraction(num) {
-    return previousValue -= num;
+function subtraction() {
+    return runningValue -= currentValue;
 }
-function multiplication(num) {
-    return previousValue *= num;
+function multiplication() {
+    return runningValue *= currentValue;
 }
-function division(num) {
-    return previousValue /= num;
+function division() {
+    if (currentValue === 0) {
+        return "DIVIDE BY 0 : WORLD IS ENDING";
+    } else {
+        return Math.round((runningValue /= currentValue) * 1000) / 1000;
+    }
+}
+
+function clearArray() {
+    array.splice(0, array.length);
+}
+
+function calculateNumber(operator) {
+    numberLine.textContent = operator();
+    console.log('Running Value: ' + runningValue);
+    console.log('Current Value: ' + currentValue);
+    pressTwice = false;
 }
